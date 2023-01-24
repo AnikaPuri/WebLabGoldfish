013f676ecdf0750716df89f4bc849e6d035dce68
s-emshi <68162832+Mineymma@users.noreply.github.com>
2023-01-24 13:29:56 -0500

    changed colors and skeleton to navbar

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,19 +1,37 @@
 import React from "react";
+import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
 import { Link } from "@reach/router";
-// import GoogleLogin, { GoogleLogout } from "react-google-login";
 
+
+import "../../utilities.css";
 import "./NavBar.css";
 
-const NavBar = (props) => {
-    return (
+//DONE REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "875626601835-nlh62o0i41iqspllgddn883q5jsatt2v.apps.googleusercontent.com";
+
+const Skeleton = ({ userId, handleLogin, handleLogout }) => {
+  return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      {userId ? (
+        <button
+          onClick={() => {
+            googleLogout();
+            handleLogout();
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+      )}
       <nav className="NavBar-container">
         <div className="NavBar-title u-inlineBlock">goldfish</div>
         <div className="NavBar-linkContainer u-inlineBlock">
           <Link to="/" className="NavBar-link">
             Explore
           </Link>
-          {props.userId && (
-            <Link to={`/profile/${props.userId}`} className="NavBar-link">
+          {userId && (
+            <Link to={`/profile/`} className="NavBar-link">
               Profile
             </Link>
           )}
@@ -21,6 +39,10 @@ const NavBar = (props) => {
             Matches
           </Link>
         </div>
-    </nav>
+      </nav>
+      <h1>Good luck on your project :)</h1>
+    </GoogleOAuthProvider>
   );
 };
+
+export default Skeleton;
